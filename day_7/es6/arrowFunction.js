8363c9a6cadedac558ba2941906b15076615cde1
Anamika yadav <anaarav246@gmail.com>
2025-09-29 15:36:23 +0530

    day 9

--- day_7/es6/arrowFunction.js
+++ day_7/es6/arrowFunction.js
@@ -1,4 +1,22 @@
-const isEven = (a) =>
-     a%2==0 ?"number is even":"number is odd";
-console.log(isEven(4));
-console.log(isEven(5));
+// const isEven = (a) =>
+//      a%2==0 ?"number is even":"number is odd";
+// console.log(isEven(4));
+// console.log(isEven(5));
+// let = 2;
+// if(a>20){
+//      let a = 90;
+//      console.log("a inside if="+a);
+// }
+//  else{
+//  cons
+//  }
+//  function greetings(name){
+//      return "Hello "+name;
+     
+//  }
+//     Data("john");
+// console.log(msg);
+// const data=()=>{
+//      let msg = "Hello World";
+//      console.log(msg);
+// }
